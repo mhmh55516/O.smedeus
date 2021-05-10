73c862ee612b50d0db3b3217fd5cd8fb57520011
mhmh55516 <mhmh55516@gmail.com>
2021-05-11 00:09:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,19 +15,19 @@ install_banner() {
 }
 
 install_banner "git, nmap, masscan, chromium, npm, golang"
-[ -x "$(command -v git)" ] || sudo $PACKGE_MANAGER install git -y 2>/dev/null
-[ -x "$(command -v nmap)" ] || sudo $PACKGE_MANAGER install nmap -y 2>/dev/null
+##[ -x "$(command -v git)" ] || sudo $PACKGE_MANAGER install git -y 2>/dev/null
+##[ -x "$(command -v nmap)" ] || sudo $PACKGE_MANAGER install nmap -y 2>/dev/null
 [ -x "$(command -v masscan)" ] || sudo $PACKGE_MANAGER install masscan -y 2>/dev/null
-[ -x "$(command -v chromium)" ] || sudo $PACKGE_MANAGER install chromium -y 2>/dev/null
+##[ -x "$(command -v chromium)" ] || sudo $PACKGE_MANAGER install chromium -y 2>/dev/null
 [ -x "$(command -v make)" ] || sudo $PACKGE_MANAGER install build-essential -y 2>/dev/null
 [ -x "$(command -v csvlook)" ] || sudo $PACKGE_MANAGER install csvkit -y 2>/dev/null
 [ -x "$(command -v rg)" ] || sudo $PACKGE_MANAGER install ripgrep -y 2>/dev/null
-[ -x "$(command -v unzip)" ] || sudo $PACKGE_MANAGER install unzip -y 2>/dev/null
-[ -x "$(command -v chromium-browser)" ] || sudo $PACKGE_MANAGER install chromium-browser -y 2>/dev/null
+##[ -x "$(command -v unzip)" ] || sudo $PACKGE_MANAGER install unzip -y 2>/dev/null
+##[ -x "$(command -v chromium-browser)" ] || sudo $PACKGE_MANAGER install chromium-browser -y 2>/dev/null
 [ -x "$(command -v xsltproc)" ] || sudo $PACKGE_MANAGER install xsltproc -y 2>/dev/null
 [ -x "$(command -v make)" ] || sudo $PACKGE_MANAGER install build-essential -y 2>/dev/null
-[ -x "$(command -v pip)" ] || sudo $PACKGE_MANAGER install python-pip -y 2>/dev/null
-[ -x "$(command -v pip3)" ] || sudo $PACKGE_MANAGER install python3-pip -y 2>/dev/null
+##[ -x "$(command -v pip)" ] || sudo $PACKGE_MANAGER install python-pip -y 2>/dev/null
+##[ -x "$(command -v pip3)" ] || sudo $PACKGE_MANAGER install python3-pip -y 2>/dev/null
 
 pip install setuptools 2>/dev/null
 pip3 install setuptools 2>/dev/null
@@ -49,36 +49,36 @@ mkdir -p "$PLUGINS_PATH/go/" 2>/dev/null
 
 # domain discovery
 
-[[ -f $DATA_PATH/wordlists/dns/all.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/all.txt https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
+##[[ -f $DATA_PATH/wordlists/dns/all.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/all.txt https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
 
-[[ -f $DATA_PATH/wordlists/dns/commonspeak2-subdomains.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/commonspeak2-subdomains.txt https://raw.githubusercontent.com/assetnote/commonspeak2-wordlists/master/subdomains/subdomains.txt
+##[[ -f $DATA_PATH/wordlists/dns/commonspeak2-subdomains.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/commonspeak2-subdomains.txt https://raw.githubusercontent.com/assetnote/commonspeak2-wordlists/master/subdomains/subdomains.txt
 
-[[ -f $DATA_PATH/wordlists/dns/shorts.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/shorts.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/subdomains-top1million-20000.txt
+##[[ -f $DATA_PATH/wordlists/dns/shorts.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/shorts.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/subdomains-top1million-20000.txt
 
 # permutation domain
-[[ -f $DATA_PATH/wordlists/dns/short-permutation.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/short-permutation.txt https://raw.githubusercontent.com/subfinder/goaltdns/master/words.txt
+##[[ -f $DATA_PATH/wordlists/dns/short-permutation.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/short-permutation.txt https://raw.githubusercontent.com/subfinder/goaltdns/master/words.txt
 
 # vhost domain
-[[ -f $DATA_PATH/wordlists/dns/virtual-host-scanning.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/virtual-host-scanning.txt https://raw.githubusercontent.com/codingo/VHostScan/master/VHostScan/wordlists/virtual-host-scanning.txt
+##[[ -f $DATA_PATH/wordlists/dns/virtual-host-scanning.txt ]] || wget -q -O $DATA_PATH/wordlists/dns/virtual-host-scanning.txt https://raw.githubusercontent.com/codingo/VHostScan/master/VHostScan/wordlists/virtual-host-scanning.txt
 
 # content discovery
-[[ -f $DATA_PATH/wordlists/content/raft-large-directories.txt ]] || wget -q -O $DATA_PATH/wordlists/content/raft-large-directories.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/raft-large-directories.txt
+##[[ -f $DATA_PATH/wordlists/content/raft-large-directories.txt ]] || wget -q -O $DATA_PATH/wordlists/content/raft-large-directories.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/raft-large-directories.txt
 
-[[ -f $DATA_PATH/wordlists/content/quick.txt ]] || wget -q -O $DATA_PATH/wordlists/content/quick.txt https://raw.githubusercontent.com/maurosoria/dirsearch/master/db/dicc.txt
+##[[ -f $DATA_PATH/wordlists/content/quick.txt ]] || wget -q -O $DATA_PATH/wordlists/content/quick.txt https://raw.githubusercontent.com/maurosoria/dirsearch/master/db/dicc.txt
 
 
-[[ -f $DATA_PATH/wordlists/content/top10000.txt ]] || wget -q -O $DATA_PATH/wordlists/content/top10000.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/RobotsDisallowed-Top1000.txt
+##[[ -f $DATA_PATH/wordlists/content/top10000.txt ]] || wget -q -O $DATA_PATH/wordlists/content/top10000.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/RobotsDisallowed-Top1000.txt
 
-cat $DATA_PATH/wordlists/content/quick.txt $DATA_PATH/wordlists/content/top10000.txt > $DATA_PATH/wordlists/content/quick-content-discovery.txt
+##cat $DATA_PATH/wordlists/content/quick.txt $DATA_PATH/wordlists/content/top10000.txt > $DATA_PATH/wordlists/content/quick-content-discovery.txt
 
-[[ -f $DATA_PATH/wordlists/content/dir-all.txt ]] || wget -q -O $DATA_PATH/wordlists/content/dir-all.txt https://gist.githubusercontent.com/jhaddix/b80ea67d85c13206125806f0828f4d10/raw/c81a34fe84731430741e0463eb6076129c20c4c0/content_discovery_all.txt
+##[[ -f $DATA_PATH/wordlists/content/dir-all.txt ]] || wget -q -O $DATA_PATH/wordlists/content/dir-all.txt https://gist.githubusercontent.com/jhaddix/b80ea67d85c13206125806f0828f4d10/raw/c81a34fe84731430741e0463eb6076129c20c4c0/content_discovery_all.txt
 
 # params
-[[ -f $DATA_PATH/wordlists/params/param-miner.txt ]] || wget -q -O $DATA_PATH/wordlists/params/param-miner.txt https://raw.githubusercontent.com/PortSwigger/param-miner/master/resources/params
+##[[ -f $DATA_PATH/wordlists/params/param-miner.txt ]] || wget -q -O $DATA_PATH/wordlists/params/param-miner.txt https://raw.githubusercontent.com/PortSwigger/param-miner/master/resources/params
 
-[[ -f $DATA_PATH/wordlists/params/parameth.txt ]] || wget -q -O $DATA_PATH/wordlists/params/parameth.txt https://raw.githubusercontent.com/maK-/parameth/master/lists/all.txt
+##[[ -f $DATA_PATH/wordlists/params/parameth.txt ]] || wget -q -O $DATA_PATH/wordlists/params/parameth.txt https://raw.githubusercontent.com/maK-/parameth/master/lists/all.txt
 
-cat $DATA_PATH/wordlists/params/param-miner.txt $DATA_PATH/wordlists/params/parameth.txt | sort -u > $DATA_PATH/wordlists/params/all.txt
+##cat $DATA_PATH/wordlists/params/param-miner.txt $DATA_PATH/wordlists/params/parameth.txt | sort -u > $DATA_PATH/wordlists/params/all.txt
 
 # Subdomain takeover signature
 install_banner "providers-data for subdomain takeover"
@@ -166,10 +166,10 @@ cd $CWD
 ##
 # Install go stuff
 ##
-install_banner "gobuster"
-$GO_BIN get -ldflags "-s -w" -u github.com/OJ/gobuster
-install_banner "aquatone"
-$GO_BIN get -ldflags "-s -w" -u github.com/michenriksen/aquatone
+##install_banner "gobuster"
+##$GO_BIN get -ldflags "-s -w" -u github.com/OJ/gobuster
+##install_banner "aquatone"
+##$GO_BIN get -ldflags "-s -w" -u github.com/michenriksen/aquatone
 install_banner "subjack"
 $GO_BIN get -ldflags "-s -w" -u github.com/haccer/subjack
 install_banner "tko-subs"
@@ -178,8 +178,8 @@ install_banner "subzy"
 $GO_BIN get -ldflags "-s -w" -u github.com/lukasikic/subzy
 install_banner "goaltdns"
 $GO_BIN get -ldflags "-s -w" -u github.com/subfinder/goaltdns
-install_banner "gowitness"
-$GO_BIN get -ldflags "-s -w" -u github.com/sensepost/gowitness
+##install_banner "gowitness"
+##$GO_BIN get -ldflags "-s -w" -u github.com/sensepost/gowitness
 install_banner "webanalyze"
 $GO_BIN get -ldflags "-s -w" -u github.com/rverton/webanalyze/cmd/webanalyze
 install_banner "assetfinder"
@@ -188,10 +188,10 @@ install_banner "waybackurls"
 $GO_BIN get -ldflags "-s -w" -u github.com/tomnomnom/waybackurls
 install_banner "meg"
 $GO_BIN get -ldflags "-s -w" -u github.com/tomnomnom/meg
-install_banner "httprobe"
-$GO_BIN get -ldflags "-s -w" -u github.com/tomnomnom/httprobe
-install_banner "unfurl"
-$GO_BIN get -ldflags "-s -w" -u github.com/tomnomnom/unfurl
+##install_banner "httprobe"
+##$GO_BIN get -ldflags "-s -w" -u github.com/tomnomnom/httprobe
+##install_banner "unfurl"
+##$GO_BIN get -ldflags "-s -w" -u github.com/tomnomnom/unfurl
 install_banner "filter-resolved"
 $GO_BIN get -ldflags "-s -w" -u github.com/tomnomnom/hacks/filter-resolved
 install_banner "ffuf"
@@ -203,8 +203,8 @@ $GO_BIN get -ldflags "-s -w" -u github.com/jaeles-project/gospider
 install_banner "go cli-utils"
 $GO_BIN get -ldflags "-s -w" -u github.com/j3ssie/go-auxs/just-resolved
 
-install_banner "gitleaks"
-GO111MODULE=on $GO_BIN get -ldflags "-s -w" github.com/zricethezav/gitleaks/v6
+##install_banner "gitleaks"
+##GO111MODULE=on $GO_BIN get -ldflags "-s -w" github.com/zricethezav/gitleaks/v6
 install_banner "amass"
 GO111MODULE=on $GO_BIN get -ldflags "-s -w" github.com/OWASP/Amass/v3/... 2>/dev/null
 install_banner "subfinder"
@@ -245,8 +245,8 @@ chmod +x $PLUGINS_PATH/findomain
 # Install python stuff
 ##
 
-install_banner "truffleHog"
-pip3 install truffleHog
+##install_banner "truffleHog"
+##pip3 install truffleHog
 
 cd $PLUGINS_PATH
 
@@ -254,8 +254,8 @@ cd $PLUGINS_PATH
 # git clone https://github.com/drwetter/testssl.sh 2>/dev/null
 
 
-install_banner "Arjun"
-git clone https://github.com/s0md3v/Arjun 2>/dev/null
+##install_banner "Arjun"
+##git clone https://github.com/s0md3v/Arjun 2>/dev/null
 
 install_banner "CORStest"
 git clone https://github.com/RUB-NDS/CORStest 2>/dev/null
